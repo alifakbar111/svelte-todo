e48133844dfb3cf8cbc7751ff913b18c145b94fb
alifakbar111 <alifakbar111@gmail.com>
2020-11-12 17:40:01 +0700

    feat: add stores file

---
+++ src/components/Alerts/index.svelte
@@ -0,0 +1,39 @@
+<script>
+  import { alert, visible } from "../stores";
+  import { onDestroy } from "svelte";
+
+  export let ms = 3000;
+  // let visible;
+  let timeout;
+
+  const onMessageChange = (message, ms) => {
+    clearTimeout(timeout);
+    if (!message) {
+      $visible = false;
+    } else {
+      $visible = true;
+      if (ms > 0) {
+        timeout = setTimeout(() => ($visible = false), ms);
+      }
+    }
+  };
+  $: onMessageChange($alert, ms);
+  onDestroy(() => setTimeout(timeout));
+</script>
+
+{#if $alert}
+  <div class="modal" class:is-active={$visible}>
+    <div class="modal-background" on:click={() => ($visible = false)}/>
+    <div class="notification is-primary">
+      <button
+        class="delete"
+        aria-label="close"
+        on:click={() => ($visible = false)} />
+      <strong>{$alert}</strong>
+    </div>
+    <button
+      class="modal-close is-large"
+      aria-label="close"
+      on:click={() => ($visible = false)} />
+  </div>
+{/if}
--- src/components/Todos/index.svelte
+++ src/components/Todos/index.svelte
@@ -6,26 +6,49 @@
     NewTodo,
     TodoStatus,
   } from "../../components";
+  import { alert } from "../stores";
 
   export let todos = [];
-  let newTodoName = "";
   let newTodoId;
-  let filterAll;
+  let filterAll = "all";
   let todoStatus;
-  
+
   $: newTodoId = todos.length ? Math.max(...todos.map((t) => t.id)) + 1 : 1;
 
   function removeTodo(todo) {
     todos = todos.filter((t) => t.id !== todo.id);
     todoStatus.focus();
+    $alert = `Todo '${todo.name}' has been deleted`;
   }
+
   function addTodo(name) {
     todos = [...todos, { id: newTodoId, name, completed: false }];
+    $alert = `Todo '${name}' has been added`;
   }
+
   function updateTodo(todo) {
     const i = todos.findIndex((t) => t.id === todo.id);
+    if (todos[i].name !== todo.name) {
+      $alert = `Todo  '${todo[i].name}' has been renamed to '${todo.name}'`;
+    }
+    if (todos[i].completed !== todo.completed) {
+      $alert = `Todo '${todo[i].name}' marked as ${
+        todo.completed ? "completed" : "active"
+      }`;
+    }
     todos[i] = { ...todos[i], ...todo };
   }
+
+  $: {
+    if (filterAll === "all") {
+      $alert = "Browsing all todos";
+    } else if (filterAll === "active") {
+      $alert = "Browsing active todos";
+    } else if (filterAll === "completed") {
+      $alert = "Browsing completed todos";
+    }
+  }
+
   const filterTodos = (filterAll, todos) =>
     filterAll === "active"
       ? todos.filter((t) => !t.completed)
@@ -34,13 +57,14 @@
       : todos;
 
   const checkAllTodos = (completed) => {
-    todos = todos.map((t) => {
-      return { ...t, completed: completed };
-    });
+    todos = todos.map((t) => ({ ...t, completed }));
+    $alert = `${completed ? "Checked" : "Unchecked"} ${todos.length} todos`;
   };
 
-  const removeCompletedTodos = () =>
-    (todos = todos.filter((t) => !t.completed));
+  const removeCompletedTodos = () => {
+    $alert = `Removed ${todos.filter((t) => t.completed).length} todos`;
+    todos = todos.filter((t) => !t.completed);
+  };
 </script>
 
 <section class="section">
